b14489cad82072ea8f8409959fd20fa23913f54d
Anthony Dillon <me@anthonydillon.com>
2015-05-20 11:17:49 +0100

    Modulised the sass and simplified the js

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,43 @@
-function init() {
-	var machineLinks = document.getElementsByClassName("machine-list__link");
-	var inspector = document.getElementById("inspector");
-	var backButton = document.getElementsByClassName("unit-list__header-back")[0];
 
+// When the page loads this sets up the basic behaviour
+function init() {
+	var inspector = document.getElementById("inspector"),
+		toUnitList = document.getElementsByClassName("js__to--unit-list"),
+		toMachineList = document.getElementsByClassName("js__to--machine-list"),
+		toErrorList = document.getElementsByClassName("js__to--error-list");
+
+	// Change the state class on the inspector to unit-listing
 	var showUnitList = function(e) {
 		e.preventDefault();
 		inspector.className = 'inspector on__unit-listing';
 	};
 
+	// Change the state class on the inspector to machine-listing
 	var showMachineList = function (e) {
 		e.preventDefault();
 		inspector.className = 'inspector on__machine-listing';
 	};
 
-	for (var i = 0; i < machineLinks.length; i++) {
-	    machineLinks[i].addEventListener('click', showUnitList, false);
+	// Change the state class on the inspector to error-listing
+	var showErrorList = function (e) {
+		e.preventDefault();
+		inspector.className = 'inspector on__error-listing';
+	};
+
+	for (var i = 0; i < toUnitList.length; i++) {
+	    toUnitList[i].addEventListener('click', showUnitList, false);
 	}
 
-	backButton.addEventListener('click', showMachineList, false);
+	for (var i = 0; i < toMachineList.length; i++) {
+	    toMachineList[i].addEventListener('click', showMachineList, false);
+	}
+
+	for (var i = 0; i < toErrorList.length; i++) {
+	    toErrorList[i].addEventListener('click', showErrorList, false);
+	}
 }
 
-
+// Add window listener to listen to load to initialise the js behaviour
 if (window.addEventListener) {
     window.addEventListener("load", init, false);
 }
