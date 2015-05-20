c91f8b5a298c5313b2f7c06060d278624cd0bddc
Anthony Dillon <me@anthonydillon.com>
2015-05-20 13:39:12 +0100

    Added pending and uncommited views

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@ function init() {
 	var inspector = document.getElementById("inspector"),
 		toUnitList = document.getElementsByClassName("js__to--unit-list"),
 		toMachineList = document.getElementsByClassName("js__to--machine-list"),
+		toPendingList = document.getElementsByClassName("js__to--pending-list"),
+		toUncommitedList = document.getElementsByClassName("js__to--uncommited-list"),
 		toErrorList = document.getElementsByClassName("js__to--error-list");
 
 	// Change the state class on the inspector to unit-listing
@@ -24,6 +26,18 @@ function init() {
 		inspector.className = 'inspector on__error-listing';
 	};
 
+	// Change the state class on the inspector to pending-listing
+	var showPendingList = function (e) {
+		e.preventDefault();
+		inspector.className = 'inspector on__pending-listing';
+	};
+
+	// Change the state class on the inspector to uncommitted-listing
+	var showUncommitedList = function (e) {
+		e.preventDefault();
+		inspector.className = 'inspector on__uncommitted-listing';
+	};
+
 	for (var i = 0; i < toUnitList.length; i++) {
 	    toUnitList[i].addEventListener('click', showUnitList, false);
 	}
@@ -34,6 +48,14 @@ function init() {
 
 	for (var i = 0; i < toErrorList.length; i++) {
 	    toErrorList[i].addEventListener('click', showErrorList, false);
+	}
+
+	for (var i = 0; i < toUncommitedList.length; i++) {
+	    toUncommitedList[i].addEventListener('click', showUncommitedList, false);
+	}
+
+	for (var i = 0; i < toPendingList.length; i++) {
+	    toPendingList[i].addEventListener('click', showPendingList, false);
 	}
 }
 
